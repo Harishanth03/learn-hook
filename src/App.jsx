8f11c26c1aed86416d2f8a8a319a1a7f4cf20bce
Harishanth03 <kandeepanharishanth@gmail.com>
2024-10-31 09:18:18 +0530

    No dependancies

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { useEffect } from "react";
 import { useState } from "react"
 
 function App() {
 
   const [count ,  setCount] = useState(0);
+
+  /*useEffect(() => {
+
+    //1: The code that we want to run
+    //2: Optional Return statment
+  }, []); //3:Dependancy <Array></Array>*/
+
+  //No dependancy Use Effect
+
+  useEffect(() => {
+
+    alert("I am not dependancy");
+
+  });
 
   return (
     <>
